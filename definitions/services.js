cab93929e25889cd8d72fe047d12342eeaf41759
Matteo Collina <hello@matteocollina.com>
2015-03-18 12:00:50 +0000

    first commit

---
+++ definitions/services.js
@@ -0,0 +1,19 @@
+// Place service container definitions here.
+
+exports.root = {
+  type: 'blank-container'
+};
+
+// Example
+//
+// exports.web = {
+//   type: 'docker',
+//     specific: {
+//       repositoryUrl: 'git@github.com:nearform/nscaledemoweb.git',
+//       execute: {
+//         // docker run <ARGS> image <EXEC>
+//         args: '-p 8000:8000 -d',
+//         exec: ''
+//       }
+//     }
+// };
---
+++ system.js
@@ -0,0 +1,19 @@
+// Define the system topology here. The topolgy should reference 
+// containers defined in definitions/services.js
+
+exports.name = 'cloudconf';
+exports.namespace = 'cloudconf';
+exports.id = '4e89e590-f5c7-4775-9e78-08a577763f4e';
+
+exports.topology = {
+  development: {
+  }
+};
+
+// Example
+//
+// exports.topology = {
+//   development: {
+//     root: ['web']
+//   }
+// };
