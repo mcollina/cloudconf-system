34b2bb111c68754d86441158489c083b6ae8a313
Matteo Collina <hello@matteocollina.com>
2015-03-18 12:01:43 +0000

    system compile

--- definitions/services.js
+++ definitions/services.js
@@ -4,16 +4,14 @@ exports.root = {
   type: 'blank-container'
 };
 
-// Example
-//
-// exports.web = {
-//   type: 'docker',
-//     specific: {
-//       repositoryUrl: 'git@github.com:nearform/nscaledemoweb.git',
-//       execute: {
-//         // docker run <ARGS> image <EXEC>
-//         args: '-p 8000:8000 -d',
-//         exec: ''
-//       }
-//     }
-// };
+exports.web = {
+  type: 'docker',
+    specific: {
+      repositoryUrl: 'git@github.com:nearform/nscaledemoweb.git',
+      execute: {
+        // docker run <ARGS> image <EXEC>
+        args: '-p 8000:8000 -d',
+        exec: ''
+      }
+    }
+};
